df2fa80a05dc016294970e19eae524a558db070a
avanish <avanishdhake1@gmail.com>
2024-07-20 19:12:21 +0530

    Identity page in Flutter

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
@@ -10,64 +11,154 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body:Padding(
-      padding: const EdgeInsets.only(top: 100.0, left: 20.0),
-      child: Column(
-        children:<Widget>[
-          Row(
-             children: <Widget>[
-              CircleAvatar(radius: 60,backgroundColor: Colors.yellowAccent, ),
-              SizedBox(width: 10,),
-              Column(
-              children: <Widget>[
-               Text("Avanish Dhake",style: TextStyle(fontSize: 30),),
-               Text("B.Tech AI 4th Year",style: TextStyle(fontSize: 30),)
-              ],    
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/walpaper.jpg"),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100.0, left: 20.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 60, backgroundImage: AssetImage("images/ava.jpg"),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Avanish Dhake",
+                        style: TextStyle(fontSize: 30,color: Colors.white),
+                      ),
+                      Text(
+                        "B.Tech AI 4th Year",
+                        style: TextStyle(fontSize: 15,color: Colors.white),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Column(
+                  children: [
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.school,
+                          size: 40,color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text("G.H.Raisoni College, Jalgaon ",
+                            style: TextStyle(fontSize: 20,color: Colors.white))
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.laptop,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "Student Management system",
+                          style: TextStyle(fontSize: 20,color: Colors.white),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.location_on_outlined,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "Nahata College Bhusawal ",
+                          style: TextStyle(fontSize: 20,color:Colors.white),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.email_outlined,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "avanishdhake1@gmail.com",
+                          style: TextStyle(fontSize: 20,color: Colors.white,),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.call,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "8793216145",
+                          style: TextStyle(fontSize: 20,color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 240,
+              ),
+              Text(
+                " ABOUT ME",
+                style: TextStyle(fontSize: 20,color: Colors.white),
+              ),
+              Text(
+                "CREATED BY AVANISH DHAKE",
+                style: TextStyle(fontSize: 15,color: Colors.white),
               )
-             ],
-         ),
-          Column(
-            children: [
-              Row(
-
-                children:<Widget> [
-                  SizedBox(width: 10,),
-                  Icon(Icons.school,size: 25,),
-                  Text("G.H.Raisoni College, Jalgaon",style: TextStyle(fontSize: 20))
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.note_alt,size: 25,),
-                  Text("Student Management system",style: TextStyle(fontSize: 20),)
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.location_on_outlined,size: 25,),
-                  Text("LOCATION",style:TextStyle(fontSize: 20), ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.email_outlined,size: 25,),
-                  Text("avanishdhake1@gmail.com",style: TextStyle(fontSize: 20),)
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.call,size: 25,),
-                  Text("8793216145",style: TextStyle(fontSize: 20),)
-                ],
-              ),
             ],
           ),
-            Text(" ABOUT ME",style: TextStyle(fontSize: 15),),
-
-           Text("CREATED BY AVANISH DHAKE",style: TextStyle(fontSize: 15),)
-         ],
-       ),
-     ),
-     );
+        ),
+      ),
+    );
   }
 }
